df26749f38fa13f160ffe08ad4d4a93792984fd8
Lolodusiji <sholaodusiji@gmail.com>
2023-09-08 17:14:02 +0100

    link redirect

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("redirectButton").addEventListener("click", function() {
+    window.location.href = "https://github.com/Lolodusiji"; // Replace with the URL you want to redirect to
+  });
+  
